f960b737c569dac13c1fdb8810aca44c0f848155
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-26 21:19:24 +0100

    Brew formula update for azbrowse version v1.1.185

--- Formula/azbrowse.rb
+++ Formula/azbrowse.rb
@@ -2,9 +2,9 @@
 class Azbrowse < Formula
   desc "An interactive CLI for browsing Azure, inspired by http://resources.azure.com/"
   homepage "https://github.com/lawrencegripper/azbrowse"
-  url "https://github.com/lawrencegripper/azbrowse/releases/download/v1.1.170/azbrowse_darwin_amd64.tar.gz"
-  version "1.1.170"
-  sha256 "e779150b16859d338b53ac69b907a7998ff45b3c97f8feec3e1ba5cbae6e6698"
+  url "https://github.com/lawrencegripper/azbrowse/releases/download/v1.1.185/azbrowse_darwin_amd64.tar.gz"
+  version "1.1.185"
+  sha256 "e9d8efa85f54c6c64a5d686ef0ad7f807087522f8ece3e6ce3dbf648dbba200e"
 
   def install
     bin.install "azbrowse"
