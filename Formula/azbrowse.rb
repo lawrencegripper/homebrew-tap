b6bd0d699c278db5d6f30fd60085e4ce3648942c
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-25 08:47:52 +0100

    Brew formula update for azbrowse version v2.0.147133572

--- Formula/azbrowse.rb
+++ Formula/azbrowse.rb
@@ -2,16 +2,16 @@
 class Azbrowse < Formula
   desc "An interactive CLI for browsing Azure, inspired by http://resources.azure.com/"
   homepage "https://github.com/lawrencegripper/azbrowse"
-  version "2.0.146299380"
+  version "2.0.147133572"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/lawrencegripper/azbrowse/releases/download/v2.0.146299380/azbrowse_darwin_amd64.tar.gz"
-    sha256 "fe0b7d1a83c9511f509bd9f5a4321457166ce1f50626c7572be9b9492bd8d4d0"
+    url "https://github.com/lawrencegripper/azbrowse/releases/download/v2.0.147133572/azbrowse_darwin_amd64.tar.gz"
+    sha256 "9cae2605815a3bba9029d6ab5f81fe4795c2c79502afb3b431b19b91c733aa96"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/lawrencegripper/azbrowse/releases/download/v2.0.146299380/azbrowse_linux_amd64.tar.gz"
-      sha256 "3c97b11d515fa637cd3b6c57fddbf76ba72e14936b17cb75c0122ec2022d134b"
+      url "https://github.com/lawrencegripper/azbrowse/releases/download/v2.0.147133572/azbrowse_linux_amd64.tar.gz"
+      sha256 "bbb13a9aeb0a178e5f64340073db921f94ddedfefcad4394eb5e7ea147f58b7f"
     end
   end
 
